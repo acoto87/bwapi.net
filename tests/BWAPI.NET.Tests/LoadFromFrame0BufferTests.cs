9dff81b8a975559c5479a02ad1429f5ca99f6c39
acoto87 <acoto87@gmail.com>
2023-01-26 16:01:13 -0600

    Add more frame0 tests

--- tests/BWAPI.NET.Tests/LoadFromFrame0BufferTests.cs
+++ tests/BWAPI.NET.Tests/LoadFromFrame0BufferTests.cs
@@ -6,15 +6,25 @@ namespace BWAPI.NET.Tests;
 public class LoadFromFrame0BufferTests
 {
     private const string ResourcesFolder = "Resources";
-    private const string TestFileName = "(2)Breaking Point.scx_frame0_buffer.bin";
 
-    [Fact]
-    public void LoadFromFrame0Buffer()
+    [Theory]
+    [InlineData("(2)Astral Balance.scm_frame0_buffer.bin")]
+    [InlineData("(2)Breaking Point.scx_frame0_buffer.bin")]
+    [InlineData("(2)Isolation.scx_frame0_buffer.bin")]
+    [InlineData("(3)Stepping Stones.scm_frame0_buffer.bin")]
+    [InlineData("(4)Arctic Station.scx_frame0_buffer.bin")]
+    [InlineData("(4)Space Debris.scm_frame0_buffer.bin")]
+    [InlineData("(5)Twilight Star.scx_frame0_buffer.bin")]
+    [InlineData("(6)Sapphire Isles.scx_frame0_buffer.bin")]
+    [InlineData("(7)Black Lotus.scx_frame0_buffer.bin")]
+    [InlineData("(8)Frozen Sea.scx_frame0_buffer.bin")]
+    public void LoadFromFrame0Buffer(string testMapName)
     {
-        using var mmf = GetMemoryMappedFileForMap(Path.Combine(ResourcesFolder, TestFileName));
+        using var mmf = GetMemoryMappedFileForMap(Path.Combine(ResourcesFolder, testMapName));
         using var gameViewAccessor = mmf.CreateViewAccessor(0, ClientData.GameData_.Size, MemoryMappedFileAccess.ReadWrite);
         var clientData = new ClientData(gameViewAccessor);
         var game = new Game(clientData);
+
         game.Init();
 
         Assert.True(game.Self().GetPlayerType() == PlayerType.Player);
